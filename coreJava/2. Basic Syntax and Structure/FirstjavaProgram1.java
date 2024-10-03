2f79536cd8be6896e3c19fc2499bc3b9a6d88840
Aman-Kumar-Gautam <amangautam862@gmail.com>
2024-10-03 10:15:38 +0530

    edited file coreJava

--- coreJava/2. Basic Syntax and Structure/FirstjavaProgram1.java
+++ coreJava/2. Basic Syntax and Structure/FirstjavaProgram1.java
@@ -1,4 +1,5 @@
-public class FirstjavaProgram1 {  /* Class define - classname(FirstJavaProgram)  */
+public class FirstjavaProgram1 {  
+    /* Class define - classname(FirstJavaProgram)  */
 
     public static void main(String[] args) {
         System.out.println("Hello World, I love 'Java'");
--- coreJava/4. Operators and Expressions/Arithmeticoperators.java
+++ coreJava/4. Operators and Expressions/Arithmeticoperators.java
@@ -1,6 +1,20 @@
 public class Arithmeticoperators {
 
     public static void main(String[] args) {
-        System.out.println(1);
+        int x = 10;
+        int y = 7;
+        int c = x+y;
+        
+        System.out.println(c);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        System.out.println(++x);
+        System.out.println(y++);
+        System.out.println(c++);
+        System.out.println(c--);
+
+        
     }
 }
