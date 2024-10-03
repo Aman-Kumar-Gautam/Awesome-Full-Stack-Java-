c830b37491bf7ceb192b7bc34077ec4d7c4a1697
Aman Gautam <62622208+Aman-Kumar-Gautam@users.noreply.github.com>
2024-10-03 11:18:24 +0530

    Update FirstjavaProgram1.java

--- coreJava/2. Basic Syntax and Structure/FirstjavaProgram1.java
+++ coreJava/2. Basic Syntax and Structure/FirstjavaProgram1.java
@@ -1,7 +1,1 @@
-public class FirstjavaProgram1 {  
-    /* Class define - classname(FirstJavaProgram)  */
 
-    public static void main(String[] args) {
-        System.out.println("Hello World, I love 'Java'");
-    }
-}
